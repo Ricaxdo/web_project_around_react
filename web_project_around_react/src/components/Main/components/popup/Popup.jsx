e592e3bcf26eeb0dc76975fbbd1b9ddf1c3c3ebc
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-10-10 03:03:08 -0600

    Creacion Popup.jsx -dos return, si es imagepopup o si es otro tipo de popup -las clases se definen por su 'variante' de popup

---
+++ web_project_around_react/src/components/Main/components/popup/Popup.jsx
@@ -0,0 +1,68 @@
+// src/components/.../popup/Popup.jsx
+import closeIcon from "../../../../images/close_icon.png";
+
+export default function Popup({
+  isOpen = true,
+  onClose,
+  title,
+  children,
+  variant, // "imagePopup" | "editPopup" | "addPopup" | "avatarPopup" | "confirmationPopup"
+}) {
+  if (!isOpen) return null;
+
+  if (variant === "imagePopup") {
+    return (
+      <div
+        className={`popup ${variant} popup_opened`}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="imagePopup__container">
+          {children}
+          <button
+            className="imagePopup__close-button"
+            type="button"
+            aria-label="Close modal"
+            onClick={onClose}
+          >
+            <img
+              src={closeIcon}
+              alt="Cerrar"
+              className="imagePopup__close-button-icon"
+            />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={`popup ${variant || ""} popup_opened`}
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="popup__container">
+        <div className="popup__content">
+          <h2 className={variant ? `${variant}__title` : "popup__title"}>
+            {title}
+          </h2>
+          {children}
+        </div>
+
+        <button
+          className={`${variant}__close-button`}
+          type="button"
+          aria-label="Close modal"
+          onClick={onClose}
+        >
+          <img
+            src={closeIcon}
+            alt="Cerrar"
+            className={`${variant}__close-button-icon`}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
