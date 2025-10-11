fb77db0a3f6b72cf81dad1197521dcbdd15e64f4
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-10-11 06:33:13 -0600

    otravez

--- web_project_around_react/src/components/Main/components/form/imagepopup/ImagePopup.jsx
+++
@@ -1,8 +0,0 @@
-export default function ImagePopup({ card }) {
-  if (!card) return null;
-  return (
-    <div className="imagePopup__container">
-      <img className="imagePopup__image" src={card.link} alt={card.name} />
-    </div>
-  );
-}
